ae233408a9e32348bba0155441c4a7776b4726dc
stephan-hof <stephan-hof@users.noreply.github.com>
2025-11-19 10:07:27 +0100

    Support a Py_LIMITED_API wheel.

--- rust/src/bindings/converters/offset_datetime.rs
+++ rust/src/bindings/converters/offset_datetime.rs
@@ -4,26 +4,24 @@ pub(crate) fn py_to_offset_datetime(
     py_obj: &Bound<pyo3::types::PyAny>
 ) -> anyhow::Result<time::OffsetDateTime> {
 
-    use pyo3::types::PyDateAccess;
-    use pyo3::types::PyTimeAccess;
-    
     let dt = match py_obj.cast::<pyo3::types::PyDateTime>() {
         Ok(dt) => dt,
         Err(_) => anyhow::bail!("Expected datetime")
     };
-    
+
+    let year: i32 = dt.getattr("year")?.extract()?;
+    let month: u8 = dt.getattr("month")?.extract()?;
+    let day: u8 = dt.getattr("day")?.extract()?;
+
+    let hour: u8 = dt.getattr("hour")?.extract()?;
+    let minute: u8 = dt.getattr("minute")?.extract()?;
+    let second: u8 = dt.getattr("second")?.extract()?;
+
+    let month= time::Month::try_from(month)?;
+
     let naive_dt = time::PrimitiveDateTime::new(
-        time::Date::from_calendar_date(
-            dt.get_year(),
-            time::Month::try_from(dt.get_month())?,
-            dt.get_day(),
-        )?,
-        time::Time::from_hms(
-            dt.get_hour(),
-            dt.get_minute(),
-            dt.get_second(),
-        )?
+        time::Date::from_calendar_date(year, month, day)?,
+        time::Time::from_hms(hour, minute, second)?
     );
     Ok(naive_dt.assume_utc())
 }
-
